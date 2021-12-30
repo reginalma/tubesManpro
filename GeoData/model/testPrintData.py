59e403c06203d0e622523b71c679c2ee14d084f2
YeremyFerellHidayat <nacc260@gmail.com>
2021-12-30 16:34:14 +0700

    python ke php udah nyambung

--- GeoData/model/testPrintData.py
+++ GeoData/model/testPrintData.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 # import socket as sc
 
 # sc.setdefaulttimeout(10)
 
-dt_weather = pd.read_csv("\model\weather_uts.csv")
+dt_weather = pd.read_csv("D:\\UtilityApp\\xampp\\htdocs\\GeoData\\model\\weather_uts.csv", delimiter=',')
 
-test = dt_weather.info()
+test = dt_weather['Location'].unique()
 print(test)
 
-print("PRINT1")
-print("PRINT2")
-print("PRINT3")
-print("PRINT4")
-print("PRINT5")
+# print("PRINT1")
+# print("PRINT2")
+# print("PRINT3")
+# print("PRINT4")
+# print("PRINT5")
